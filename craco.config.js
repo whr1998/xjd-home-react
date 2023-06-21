d851969a7551ba529da694a4aac65c11bba7c877
xjd <2973347293@qq.com>
2023-06-21 09:10:18 +0800

    setting global sass import

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const cracoPluginStyleResourcesLoader = require('craco-plugin-stylus-resources-loader')
 
 module.exports = {
   // webpack 配置
@@ -12,5 +13,28 @@ module.exports = {
       '@pages': path.resolve(__dirname, 'src/pages'),
       '@routes': path.resolve(__dirname, 'src/routes'),
     }
-  }
+  },
+
+   // plugins配置
+   plugins: [
+    // 配置全局scss
+    {
+      plugin: cracoPluginStyleResourcesLoader,
+      options: {
+        patterns: [
+          path.join(__dirname, 'src/styles/_handleThemes.scss'),
+          path.join(__dirname, 'src/styles/_mixin.scss'),
+          path.join(__dirname, 'src/styles/_variable.scss'),
+        ],
+        /*
+            Please enter supported CSS processor type
+            1. if u use css processor，please type css string
+            2. if u use less processor，please type less string
+            3. if u use sass or scss processor，please type sass or scss string，Choose one of the two
+            4. if u use stylus processor，please type stylus string
+        */
+        styleType: 'sass',
+      },
+    },
+  ],
 }
